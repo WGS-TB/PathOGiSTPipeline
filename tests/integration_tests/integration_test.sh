5811820e9b4505a14b3c2c07dee4767f7b85d1b9
jowong4 <34543031+jowong4@users.noreply.github.com>
2019-03-24 12:57:27 -0700

    Update integration_test.sh

--- tests/integration_tests/integration_test.sh
+++ tests/integration_tests/integration_test.sh
@@ -12,6 +12,7 @@ conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
 conda install kwip --yes
+free -m
 kwip -d tests/integration_tests/temp_dir/kwip_dist.tsv tests/integration_tests/temp_dir/SRR6152717.ct.gz tests/integration_tests/temp_dir/SRR6153024.ct.gz
 : '
 #conda install conda-build --yes
