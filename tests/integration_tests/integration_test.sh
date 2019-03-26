0b5a2eaa4aecf6e63a8df5e4e75cf4ae4371e3e1
jowong4 <34543031+jowong4@users.noreply.github.com>
2019-03-25 20:09:24 -0700

    Update integration_test.sh

--- tests/integration_tests/integration_test.sh
+++ tests/integration_tests/integration_test.sh
@@ -24,17 +24,16 @@ conda install pathogist --yes -q
 
 
 PATHOGIST run tests/integration_tests/test_data/pathogist-run_all-test.yaml 
-
 python -m unittest tests/integration_tests/test_integration.py
-# Run Genotyping Software
-./PATHOGIST run tests/integration_tests/test2_data/pathogist-run_all-test2.yaml 
 
-python -m unittest tests/integration_tests/test2_integration.py
-./PATHOGIST run tests/integration_tests/test3_data/pathogist-run_all-test3.yaml 
-
+# Run Genotyped File to Clustering
+PATHOGIST run tests/integration_tests/test3_data/pathogist-run_all-test3.yaml 
 python -m unittest tests/integration_tests/test3_integration.py
 
-./PATHOGIST run tests/integration_tests/test4_data/pathogist-run_all-test4.yaml 
-
+# Run Distance matrices to Clustering
+PATHOGIST run tests/integration_tests/test4_data/pathogist-run_all-test4.yaml 
 python -m unittest tests/integration_tests/test4_integration.py
 
+# Run Genotyping Software
+PATHOGIST run tests/integration_tests/test2_data/pathogist-run_all-test2.yaml 
+python -m unittest tests/integration_tests/test2_integration.py
